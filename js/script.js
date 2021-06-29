f5cf5fb07ee0417b1c39accbb7165a5cf6069dab
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-29 16:15:55 +0200

    struttura

---
+++ js/script.js
@@ -0,0 +1,33 @@
+Vue.config.devtools = true;
+
+new Vue({
+
+    el:'#app',
+    data:{
+
+      toDoList:[
+          'Fare lo yoga',
+          'Chiamare ai miei',
+          'Fare la spesa'
+      ],
+        newTask:'',
+    },
+    mounted(){
+        this.autofocus();
+    },
+    methods:{
+        autofocus: function(){
+            const element = document.getElementById('newTaskElement');
+            element.focus();
+        },
+        addTask: function(){
+            this.toDoList.push(this.newTask);
+            this.newTask='';
+            this.autofocus();
+        },
+        deleteTask: function (index){
+            this.toDoList.splice(index, 1),
+            this.autofocus();
+        }
+    } 
+})
